38c8489c5a53866cb40a6029c45c278d8ce9743b
rizalvalry <rizal.muh.rzl@gmail.com>
2021-07-18 22:22:09 +0700

    pixel

--- media.php
+++ media.php
@@ -134,6 +134,25 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         <!-- <script async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDK2Axt8xiFYMBMDwwG1XzBQvEbYpzCvFU"></script> -->
         
     </main>
+
+        <!-- Facebook Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '385044909611182');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=385044909611182&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Facebook Pixel Code -->
+
     <!--  JavaScripts
 =============================================-->
     <script src="assets/lib/jquery/dist/jquery.js"></script>
